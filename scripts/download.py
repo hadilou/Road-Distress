71a5b77370b2179a820af90a3f641177ed9f355a
Kayode hadilou ADJE <kayodehadilou@hotmail.com>
2020-04-24 00:45:38 +0300

    load chunks added

--- scripts/download.py
+++ scripts/download.py
@@ -1,8 +1,13 @@
+
+"""
+@author Kayode H. ADJE
+		kaadje@ttu.ee
+"""
 from gaps_dataset import gaps
+
 gaps.download(login='gapsro2s;i2A*7',
 	output_dir ='Dataset',
 	version=2,
 	patchsize=160,
-	issue='NORMvsDISTRESS_50k''
-	debug_outputs=True)
-
+	issue='NORMvsDISTRESS_50k',
+	debug_outputs =True)
