0545f0e0a876c7e34d2b516aff2a71b7e512b1f3
Kayode hadilou ADJE <kayodehadilou@hotmail.com>
2020-04-25 02:00:03 +0300

    crop and save completed

--- scripts/segmentationPre.py
+++ scripts/segmentationPre.py
@@ -8,27 +8,92 @@ import numpy as np
 from PIL import Image
 import os
 import glob
+import cv2
+import argparse
 
-test_img_link = "../Dataset/v2/segmentation/images/_cam12_1431182332579_000743.jpg"
+test_img_link = "../Dataset/v2/segmentation/test/_cam11_1431182256923_000012.png"
 test_folder = "../Dataset/test"
-def crop(im,desiredHeight,desiredWidth):
-    """ 
-    Crop given image to desired height and width
 
+def cropAndSave(im,output_dir,desiredHeight=180,desiredWidth=320):
+    """ 
+    Crop given image to desired height and width and save to desired folder
+
+    Parameters
+    -----
+    im: Image, directory where images to crop exist
+
+    desiredHeight: int, desired height, default 320
+
+    desiredWidth: int, desired width, default 180
+
+    output_dir: String, path of the folder where to save out to.
+
+    Returns
+    ----- 
+    out: Image, yield  of croped images
     """
     imgwidth, imgheight = im.size
+    #make sure directory exists
+    if (os.path.isdir(output_dir)==False):
+        os.mkdir(output_dir)
     for i in range(imgheight//desiredHeight):
         for j in range(imgwidth//desiredWidth):
+            f_name,f_extension = os.path.splitext(im.filename)
+            f_name = substract(f_name,os.path.dirname(f_name))
             box = (j*desiredWidth, i*desiredHeight, (j+1)*desiredWidth, (i+1)*desiredHeight)
             out = im.crop(box)
-            #out.show()
-            yield out
+            out = np.array(out)
+            print(output_dir)
+            path = output_dir+f_name+'_'+str(i)+'_'+str(j)+f_extension
+            print("Saving cropped image to "+path)
+            cv2.imwrite(path,out)
+            #yield out
 
-im = Image.open(test_img_link)
+def substract(a,b):
+    """Substract two strings
+    Parameter
+    -----
+    a: String, first string to substract from
 
-crop(im,320,240)
+    b: String, seconf string to substract from a
 
+    Returns
+    -----
+    out: String, a-b
+    """
+    out = "".join(a.rsplit(b))
+    return out
 
+def main(input_dir,output_dir):
+    """
+    Main function
+    """
+    #browse inputdir , crop and save images
+    for file in os.listdir(input_dir):
+        if file.lower().endswith((".jpg",".png",".PNG",".JPG",".jpeg",".JPEG")):
+            try:
+                im = Image.open(os.path.join(input_dir,file))
+                cropAndSave(im,output_dir) 
+            except Exception as e:
+                print(e)
+        else:#file not image ignore
+            pass
+    
+if __name__=='__main__':
+    #Parsing arguments
+    ap = argparse.ArgumentParser()
+    ap.add_argument("-i","--inputdir",required=True,help="Path to input directory")
+    ap.add_argument("-o","--outputdir",required=True,help="Path to output directory")
+    ap.add_argument("-w","--width",required=False,help="Desired Width, default 180")
+    ap.add_argument("-l","--height",required=False,help="Desired Height, default 320")
+
+    args = vars(ap.parse_args())
+    input_dir = args["inputdir"]
+    output_dir = args["outputdir"]
+    desired_h = args["height"]
+    desired_w = args["width"]
+    #main function call
+    main(input_dir,output_dir)
 
 
 
